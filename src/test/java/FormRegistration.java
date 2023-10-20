8669f1dcd731ad57506c02ae46baf8ed7829d9ad
Daria9797 <144155239+Daria9797@users.noreply.github.com>
2023-10-20 22:00:29 +0300

    Update FormRegistration.java

--- src/test/java/FormRegistration.java
+++ src/test/java/FormRegistration.java
@@ -4,32 +4,27 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.junit.jupiter.api.Tag;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
 
-@Tag("demoqa")
+
 public class FormRegistration {
-      @BeforeAll
-    static void beforeAll() {
-        Configuration.baseUrl = "https://demoqa.com";
+    @BeforeAll
+    static void beforeall()
+    {
+        Configuration.baseUrl="https://demoqa.com";
+        Configuration.pageLoadStrategy="eager";
         Configuration.browserSize = "1920x1080";
-//        Configuration.browser = "chrome";
-        Configuration.timeout = 10000;
-//        Configuration.holdBrowserOpen = true;
+        //Configuration.holdBrowserOpen=true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-
+        
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
+
     }
 
     @AfterEach
