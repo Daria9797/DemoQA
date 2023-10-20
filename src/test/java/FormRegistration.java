e6510661dabecb903216f2d3740d793bcaede029
Daria9797 <144155239+Daria9797@users.noreply.github.com>
2023-10-20 21:25:51 +0300

    Update FormRegistration.java

--- src/test/java/FormRegistration.java
+++ src/test/java/FormRegistration.java
@@ -9,7 +9,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-
+@Tag("demoqa")
 public class FormRegistration {
     @BeforeAll
     static void beforeall()
