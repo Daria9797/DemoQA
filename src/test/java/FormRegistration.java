b6d9ba885b72870186ecbbce9b6a03f190f560db
Daria9797 <144155239+Daria9797@users.noreply.github.com>
2023-10-20 22:00:47 +0300

    Update FormRegistration.java

--- src/test/java/FormRegistration.java
+++ src/test/java/FormRegistration.java
@@ -1,5 +1,7 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -19,7 +21,7 @@ public class FormRegistration {
         Configuration.browserSize = "1920x1080";
         //Configuration.holdBrowserOpen=true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-        
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
