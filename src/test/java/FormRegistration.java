f25fb46a58106de113c6849d86e7545df99b7a73
Daria Kuteynikova <kolohmatowa@gmail.com>
2023-10-20 20:43:42 +0300

    settings for jenkins

--- src/test/java/FormRegistration.java
+++ src/test/java/FormRegistration.java
@@ -1,6 +1,9 @@
 import com.codeborne.selenide.Configuration;
+import helpers.Attach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
@@ -15,6 +18,21 @@ public class FormRegistration {
         Configuration.pageLoadStrategy="eager";
         Configuration.browserSize = "1920x1080";
         //Configuration.holdBrowserOpen=true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
     @Test
     void enterFormTest()
