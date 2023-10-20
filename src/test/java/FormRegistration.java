a177a99272f8e7c5b162cdfb31efbdde91351857
Daria Kuteynikova <kolohmatowa@gmail.com>
2023-10-20 20:24:14 +0300

    first commit

---
+++ src/test/java/FormRegistration.java
@@ -0,0 +1,59 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class FormRegistration {
+    @BeforeAll
+    static void beforeall()
+    {
+        Configuration.baseUrl="https://demoqa.com";
+        Configuration.pageLoadStrategy="eager";
+        Configuration.browserSize = "1920x1080";
+        //Configuration.holdBrowserOpen=true;
+    }
+    @Test
+    void enterFormTest()
+    {   open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+        $("#firstName").setValue("Daria");
+        $("#lastName").setValue("Kuteynikova");
+        $("#userEmail").setValue("kolohmatova@yandex.ru");
+        $("#genterWrapper").$(byText("Female")).click();
+        $("#userNumber").setValue("9002172121");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("January");
+        $(".react-datepicker__year-select").selectOption("1997");
+        $(".react-datepicker__day--006").click();
+        $("#subjectsInput").setValue("Maths").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#uploadPicture").uploadFromClasspath("cat.jpg");
+        $("#currentAddress").setValue("Moskow");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("Haryana")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Karnal")).click();
+        $("#submit").click();
+
+        $(".table-responsive").shouldHave(text("Daria Kuteynikova"));
+        $(".table-responsive").shouldHave(text("kolohmatova@yandex.ru"));
+        $(".table-responsive").shouldHave(text("Female"));
+        $(".table-responsive").shouldHave(text("9002172121"));
+        $(".table-responsive").shouldHave(text("6 January,1997"));
+        $(".table-responsive").shouldHave(text("Maths"));
+        $(".table-responsive").shouldHave(text("Sports, Reading"));
+        $(".table-responsive").shouldHave(text("cat.jpg"));
+        $(".table-responsive").shouldHave(text("Moskow"));
+        $(".table-responsive").shouldHave(text("Haryana Karnal"));
+
+
+
+
+    }
+}
