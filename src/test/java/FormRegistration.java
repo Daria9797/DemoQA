33f42579a7c110fcc5ad84dbef249c328943e4a2
Daria9797 <144155239+Daria9797@users.noreply.github.com>
2023-10-20 21:52:25 +0300

    Update FormRegistration.java

--- src/test/java/FormRegistration.java
+++ src/test/java/FormRegistration.java
@@ -11,22 +11,23 @@ import static com.codeborne.selenide.Selenide.*;
 
 @Tag("demoqa")
 public class FormRegistration {
-    @BeforeAll
-    static void beforeall()
-    {
-        Configuration.baseUrl="https://demoqa.com";
-        Configuration.pageLoadStrategy="eager";
+      @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-     
+//        Configuration.browser = "chrome";
+        Configuration.timeout = 10000;
+//        Configuration.holdBrowserOpen = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
-       DesiredCapabilities capabilities = new DesiredCapabilities();
-    capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-            "enableVNC", true,
-            "enableVideo", true
-    ));
-    Configuration.browserCapabilities = capabilities;
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
